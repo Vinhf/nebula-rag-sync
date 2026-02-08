98387736791702d72a6fe0273977c3323c5c136b
Vinhf <thanhvinh1662003@gmail.com>
2026-02-08 22:40:21 +0700

    .

--- app.py
+++ app.py
@@ -76,8 +76,8 @@ def get_vectorstore():
         docs, _ = load_articles_with_metadata()
 
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=800,
-            chunk_overlap=200
+            chunk_size=1500,
+            chunk_overlap=100
         )
         chunks = text_splitter.split_documents(docs)
 
